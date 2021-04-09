22b0c19cfee0f9adc7175f1aaa8bfc88d59fdbf7
its-nedum <de.magnificent1@gmail.com>
2021-04-09 16:51:53 +0100

    feature(added some validation checks)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// assign number divs to variables
 const n1 = document.getElementById('one');
 const n2 = document.getElementById('two');
 const n3 = document.getElementById('three');
@@ -10,7 +11,7 @@ const n9 = document.getElementById('nine');
 const n0 = document.getElementById('zero');
 const ndot = document.getElementById('dot');
 
-// operator
+// operators
 const divide = document.getElementById('divide');
 const times = document.getElementById('times');
 const minus = document.getElementById('minus');
@@ -24,6 +25,8 @@ const answer = document.getElementById('result');
 
 // clear div
 const clear = document.getElementById('clear');
+
+// clearing all items on click
 clear.addEventListener('click', () => {
     // reset store
     before = [];
@@ -137,31 +140,30 @@ equal.addEventListener('click', () => {
     let operand1 = parseFloat(before.join().replace(/,/g, ''));
     // clean up the after array
     let operand2 = parseFloat(after.join().replace(/,/g, ''));
-    // calculate
-    switch (operator) {
-        case '/':
-            result = operand1 / operand2
-            break;
-        case '*':
-            result = operand1 * operand2
-            break;
-        case '-':
-            result = operand1 - operand2
-            break;
-        default:
-            result = operand1 + operand2
-            break;
-    }
-
-    if (result === NaN) {
-        answer.innerText = 0;
-    }
-
-    answer.innerText = result;
-
-    // reset store
-    before = [];
-    after = [];
-    operator = null;
     
+    // only execute when there is operands to work with
+    if (operand1 && operand2){
+        // calculate
+        switch (operator) {
+            case '/':
+                result = operand1 / operand2
+                break;
+            case '*':
+                result = operand1 * operand2
+                break;
+            case '-':
+                result = operand1 - operand2
+                break;
+            default:
+                result = operand1 + operand2
+                break;
+        }
+
+        answer.innerText = result;
+
+        // reset store
+        before = [];
+        after = [];
+        operator = null;
+    }  
 });
